66b60524a324377c1ce7d5d82218be3912bf1714
vvbudnichenko <budnichik@gmail.com>
2024-07-09 19:54:29 +0300

    Перегрузка конструктора [#173890]

--- src/test/java/ru/job4j/TrackerTest.java
+++ src/test/java/ru/job4j/TrackerTest.java
@@ -1,6 +1,9 @@
 package ru.job4j;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrackerTest {
