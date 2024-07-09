2fa45726271330e58e22019c1e1fe96c78448a3d
vvbudnichenko <budnichik@gmail.com>
2024-07-09 17:13:55 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,14 +2,36 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void nickname(String catName) {
+        this.name = catName;
+    }
+
     public String sound() {
-        String voice = "may-may";
-        return voice;
+        return  "may-may";
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.nickname("Gav");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.nickname("Black");
+        black.show();
     }
 }
