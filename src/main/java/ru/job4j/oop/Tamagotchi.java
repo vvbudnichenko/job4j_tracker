1464b1e97857ab51f8dbeea9f065f32aa15a5070
vvbudnichenko <budnichik@gmail.com>
2024-08-24 17:43:44 +0300

    update

--- src/main/java/ru/job4j/oop/Tamagotchi.java
+++ src/main/java/ru/job4j/oop/Tamagotchi.java
@@ -1,33 +1,16 @@
 package ru.job4j.oop;
 
 public class Tamagotchi {
-    public static void ert(String[] args) {
+    public static void main(String[] args) {
         String source = "+!?";
-        char symbol1 = source.charAt(0);
-        char symbol2 = source.charAt(1);
-        char symbol3 = source.charAt(2);
-        for (int i = 0; i < source.length(); i++) {
-            if (source.charAt(i) == symbol1) {
-                System.out.print(symbol1);
-                System.out.print(symbol1);
-                System.out.println(symbol1);
-            } else if (source.charAt(i) == symbol2) {
-                System.out.print(symbol2);
-                System.out.print(symbol2);
-                System.out.print(symbol2);
-                System.out.println(symbol2);
-            } else if (source.charAt(i) == symbol3) {
-                System.out.print(symbol3);
-                System.out.println(symbol3);
+        for (int i = 0; i < source.toCharArray().length; i++) {
+            if (i == 0) {
+                System.out.println("" + source.charAt(i) + source.charAt(i) + source.charAt(i));
+            } else if (i == 1) {
+                System.out.println("" + source.charAt(i) + source.charAt(i) + source.charAt(i) + source.charAt(i));
+            } else {
+                System.out.print("" + source.charAt(i) + source.charAt(i));
             }
         }
-    }
-
-    public static void getGreeting(String name, int age) {
-        System.out.println(String.format("Привет! Я %s, мне столько лет: %d", name, age));
-    }
-
-    public static void main(String[] args) {
-        getGreeting(" ", 126);
     }
 }
