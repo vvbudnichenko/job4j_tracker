4c4966e5adc01d9bbb478ebcddafebacb4b5db50
vvbudnichenko <budnichik@gmail.com>
2024-08-28 13:40:51 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.tracker.input;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInputFive() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"5"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(5);
+    }
+
+    @Test
+    void whenValidInputSomeTimes() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"1", "2", "5", "3", "4", "6"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(2);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(5);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(3);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(4);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(6);
+    }
+
+    @Test
+    void whenInputValueIsNegative() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"-1", "-5"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(-1);
+        selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(-5);
+    }
+}
