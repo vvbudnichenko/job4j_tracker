22b5ca4eaad66e5e79f38e7b76df85f668dea3e3
vvbudnichenko <budnichik@gmail.com>
2024-09-07 21:17:20 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094 #511541]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -12,33 +12,5 @@ public class UsageArrayList {
         for (String value : names) {
             System.out.println(value);
         }
-        int a = 1;
-        Integer b;
-        Integer c;
-        b = Integer.valueOf(a);
-        c = a;
-        System.out.println("First: " + b + " Second: " + c);
-        num(Integer.valueOf(a));
-        Integer d = 5;
-        int f = d;
-        System.out.println("int f = " + f);
-        Integer m = 5;
-        int n = m.intValue();
-        System.out.println("int n = " + n);
-        Integer k = 5;
-        Integer l = 6;
-        System.out.println(k.intValue() * l.intValue());
-        System.out.println("Auto Unboxing:  " + k * l);
-        Integer g  = 250;
-        Integer h = 250;
-        System.out.println(g == h);
-    }
-
-    static void num(Integer num) {
-        System.out.println("Value of type is: " + num);
-    }
-
-    static void num(int num) {
-        System.out.println("Parent value of type is: " + num);
     }
 }
