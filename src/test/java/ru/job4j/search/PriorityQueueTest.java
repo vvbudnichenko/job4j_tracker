4182bf71f59d1c1f2d95be03bf9812a25dc8486b
vvbudnichenko <budnichik@gmail.com>
2024-09-07 21:09:03 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPrioritySecond() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("urgent");
+    }
+
+    @Test
+    public void whenHigherPriorityEquals() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 5));
+        queue.put(new Task("middle", 5));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("low");
+    }
+
+    @Test
+    public void whenTaskIsEmpty() {
+        PriorityQueue queue = new PriorityQueue();
+        Task result = queue.take();
+        assertThat(result).isNull();
+    }
+}
