8d14ad08ccaf9b9827d75b0469ee92561f4faf6d
vvbudnichenko <budnichik@gmail.com>
2024-09-21 22:13:56 +0300

    Update LexSort

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
-        String[] numberLeft = left.split("/");
-        String[] numberRight = right.split("/");
+        String[] numberLeft = left.split("\\.");
+        String[] numberRight = right.split("\\.");
         return Integer.compare(Integer.parseInt(numberLeft[0]), Integer.parseInt(numberRight[0]));
     }
 }
