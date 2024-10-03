4e1abfd1ca881cd9cb0f16c36a4889a5c2f2df6a
vvbudnichenko <budnichik@gmail.com>
2024-10-03 23:05:45 +0300

    3. Optional - empty, of [#214711]

---
+++ src/test/java/ru/job4j/map/CollegeTest.java
@@ -0,0 +1,103 @@
+package ru.job4j.map;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CollegeTest {
+    @Test
+    public void whenAccountIsOptionalEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findByAccount("000010")).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void whenAccountIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Optional<Student> student = Optional.of(new Student("Student1", "000001", "201-18-15"));
+        assertThat(college.findByAccount("000001")).isEqualTo(student);
+    }
+
+    @Test
+    public void whenSubjectIsOptionalEmptyWithNotFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findBySubjectName("000010", "Sociology")).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void whenSubjectIsOptionalEmptyWithFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findBySubjectName("000001", "Sociology")).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void whenSubjectIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Optional<Subject> subject = Optional.of(new Subject("Sociology", 65));
+        assertThat(college.findBySubjectName("000002", "Sociology")).isEqualTo(subject);
+    }
+}
